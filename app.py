02f6be4a7663f68bc89f87ddb140c2a37438b51b
arthur-pagotto <arthur.pagotto@outlook.com>
2026-09-24 16:12:44 -0300

    Alterações da aula de back-end

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 from datetime import datetime
+from models import Pessoa, db_session
+from sqlalchemy.exc import SQLAlchemyError
 
 app = Flask(__name__)
-
-activity_list = []
-suggestion_list = []
+app.config['SECRET_KEY'] = 'lanalindaperfeitamaravilhosa'
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
@@ -47,6 +47,30 @@ def listar_atividades():
 @app.route('/pessoa')
 def pessoa():
     return render_template('pessoa.html')
+
+@app.route('/pessoa/criar', methods=['GET', 'POST'])
+def criar_pessoa():
+    if request.method == 'POST':
+        nome_form = request.form.get('form_nome')
+        email_form = request.form.get('form_email')
+        senha_form = request.form.get('form_senha')
+        if not nome_form:
+            flash('Preencha o nome', 'error')
+            return render_template('criar_pessoa.html')
+        try:
+            nova_pessoa = Pessoa(nome_pessoa=nome_form, email=email_form, senha_hash=senha_form)
+            db_session.add(nova_pessoa)
+            db_session.commit()
+            return render_template('pessoa.html')
+        except SQLAlchemyError:
+            db_session.rollback()
+            flash('Erro ao salvar pessoa no bando de dados', 'error')
+            return render_template('criar_pessoa.html')
+        except:
+            db_session.rollback()
+            flash('Erro inesperado', 'error')
+            return render_template('criar_pessoa.html')
+    return render_template('criar_pessoa.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5001)
--- models.py
+++ models.py
@@ -1,9 +1,9 @@
 from sqlalchemy import create_engine, Column, Integer, String, Date, ForeignKey, func
-from sqlalchemy.orm import relationship, sessionmake, declarative_base
+from sqlalchemy.orm import relationship, sessionmaker, declarative_base, scoped_session
 
-engine = create_engine('mysql+pymysql://root:root@localhost:3306/taskflow')
+engine = create_engine('mysql+pymysql://root:senaisp@localhost:3306/taskflow')
 
-db_session = sessionmaker(bind=engine)
+db_session = scoped_session(sessionmaker(bind=engine))
 
 Base = declarative_base()
 
@@ -26,10 +26,16 @@ class Atividade(Base):
     data_execucao = Column(Date, nullable=False, server_default=func.now())
     descricao_atividade = Column(String(10000), nullable=False)
     prioridade = Column(String(10), nullable=False)
-    responsavel = Column(int, nullable=False, ForeignKey('pessoas.id_pessoa'))
+    responsavel = Column(Integer, ForeignKey('pessoas.id_pessoa'))
 
     def __repr__(self):
         return f'Atividade {self.nome_atividade}, Data Execução {self.data_execucao}, Descrição {self.descricao_atividade}, Responsavel {self.responsavel}, Prioridade {self.prioridade}'
+
+class Tipo(Base):
+    __tablename__ = 'tipos'
+    id_tipo = Column(Integer, primary_key=True)
+    nome_tipo = Column(String(100), nullable=False)
+    descricao_tipo = Column(String(10000), nullable=False)
 
 class Recurso(Base):
     __tablename__ = 'recursos'
